7ce262610f32eb6204705c55baa9ddbe9c937ce4
Romain Le Polh <romain@lengow.com>
2014-01-09 17:15:47 +0100

    Delete log when order status is not available

--- models/lengow.core.class.php
+++ models/lengow.core.class.php
@@ -883,6 +883,17 @@ class LengowCoreAbstract {
         }
     }
 
+    public static function deleteProcessOrder($lengow_order_id = null) {
+        if(is_null($lengow_order_id))
+            return false;
+
+        $db = Db::getInstance();
+
+        $sql = 'DELETE FROM ' . _DB_PREFIX_ . 'lengow_logs_import WHERE lengow_order_id = \'' . $lengow_order_id . '\' LIMIT 1';
+
+        return $db->execute($sql);
+    }
+
     /**
      * Set flag to 0 for order in process
      *
