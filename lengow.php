d8a020957e84101f725c7bcf35d37743cd2c92d5
Romain Le Polh <romain@lengow.com>
2014-01-29 09:01:09 +0100

    Update version

--- lengow.php
+++ lengow.php
@@ -69,7 +69,7 @@ class Lengow extends Module {
     public function __construct() {
         $this->name = 'lengow';
         $this->tab = 'export';
-        $this->version = '2.0.4';
+        $this->version = '2.0.4.1';
         $this->author = 'Lengow';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.4', 'max' => '1.6');
