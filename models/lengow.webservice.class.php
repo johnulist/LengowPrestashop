88da313d1d4cd1014da858cdbbcdaf2d1b1643fe
Romain Le Polh <romain@lengow.com>
2014-05-14 14:43:09 +0200

    Webservice migration v1 v2

---
+++ models/lengow.webservice.class.php
@@ -0,0 +1,205 @@
+<?php
+
+/**
+ * Copyright 2013 Lengow.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once 'lengow.check.class.php';
+require_once 'lengow.core.class.php';
+require_once 'lengow.product.class.php';
+
+/**
+ * The Lengow Webservice Class.
+ *
+ * @author Romain Le Polh <romain@lengow.com>
+ * @copyright 2013 Lengow SAS
+ */
+class LengowWebservice {
+
+    public static $webservice_url = '/modules/lengow/webservice/lengow.php?action=#ACTION';
+
+    private static $AVAILABLE_ACTION = array(
+        'logs' => 'Show import logs',
+        'check' => 'Show checklist configuration',
+        'migrate' => 'Migrate selection of products from v1 module to v2',
+    );
+
+    /**
+     * [_getUrlWebservice description]
+     * @param  string $action
+     * @return string
+     */
+    private static function _getUrlWebservice($action) {
+        if(array_key_exists($action, self::$AVAILABLE_ACTION))
+            return str_replace('#ACTION', $action, self::$webservice_url);
+        else 
+            return 'Action not available';
+    }
+
+    /**
+     * Get all action available with webservice
+     * @return void
+     */
+    public static function showAvailableAction() {
+        $out = '';
+        $out .= '# Lengow Webservice<br /><br />';
+        $i = 0;
+        foreach(self::$AVAILABLE_ACTION as $action => $description) {
+            $i++;
+            $out .= $i . ' - <a href="' . self::_getUrlWebservice($action) . '">' . $description . '</a>';
+            $out .= '<br />';
+        }
+        echo $out;
+    }
+
+    /**
+     * Check if action exists
+     * @param  string $action
+     * @return mixed
+     */
+    public static function checkAction($action) {
+        if($action == '')
+            throw new Exception('No action specified.');
+
+        if(array_key_exists($action, self::$AVAILABLE_ACTION))
+            return true;
+        else
+            throw new Exception('Unknow action.');
+    }
+
+    /**
+     * Migrate selection of products
+     * @return boolean
+     */
+    public static function migrateProductSelection() {
+        $old_products = Db::getInstance()->ExecuteS('SELECT `parametre_valeur` FROM ' . _DB_PREFIX_ . 'parametre_lengow WHERE `parametre_nom` = "product_id"');
+        if($old_products) {
+            foreach($old_products as $row) {
+                LengowProduct::publish($row['parametre_valeur'], 1);
+            }
+        }
+        echo 'Success';
+    }
+
+    /**
+     * Execute webservice action
+     * @param  string $action
+     * @return void
+     */
+    public static function execute($action) {
+        switch ($action) {
+            case 'migrate':
+                self::migrateProductSelection();
+                break;
+            case 'check':
+                if(Tools::getValue('format') == 'json') {
+                    header('Content-Type: application/json');
+                    echo LengowCheck::getJsonCheckList();
+                } else {
+                    echo "<h1>Lengow check configuration<h1>";
+                    echo LengowCheck::getHtmlCheckList();
+                }
+                break;
+            case 'logs':
+                $days = 10;
+                $show_extra = false;
+                if(Tools::getValue('delete') != '')
+                    LengowCore::deleteProcessOrder(Tools::getValue('delete'));
+                if(Tools::getValue('days') != '')
+                    $days = Tools::getValue('days');
+                if(Tools::getValue('show_extra') == 1)
+                    $show_extra = true;
+                echo LengowCheck::getHtmlLogs($days, $show_extra);
+                break;
+            case 'tax':
+                $id_order = Tools::getValue('id_order');
+                $rate = Tools::getValue('rate');
+
+                if($rate == '')
+                    die('No rate');
+                if($id_order == '')
+                    die('No order in parameters');
+
+                $order = new LengowOrder($id_order);
+                if($order->getTaxesAverageUsed() == 19.6 || $order->getTaxesAverageUsed() == 20) {
+
+                    $rate = 1 + ($rate / 100);
+                    $order->total_products = Tools::ps_round($order->total_products_wt / $rate, 2);
+                    if(_PS_VERSION_ >= '1.5') {
+                        $order->total_paid_tax_excl = Tools::ps_round($order->total_paid_tax_incl / $rate, 2);
+                        $order->total_shipping_tax_excl = Tools::ps_round($order->total_shipping_tax_incl / $rate, 2);
+                        $order->total_wrapping_tax_excl = Tools::ps_round($order->total_wrapping_tax_incl / $rate, 2);
+
+                        // Update Order Carrier
+                        $sql = 'UPDATE `' . _DB_PREFIX_ . 'order_carrier`
+                                SET `shipping_cost_tax_excl` = `shipping_cost_tax_incl` / ' . $rate . '
+                                WHERE `id_order` = ' . $id_order . '
+                                LIMIT 1';
+                        Db::getInstance()->execute($sql);
+                    }
+                    $order->update();
+
+                    // Update Order Detail
+                    if(_PS_VERSION_ >= '1.5')
+                        $order_detail = $order->getOrderDetailList();
+                    else
+                        $order_detail = $order->getProductsDetail();
+
+                    foreach($order_detail as $detail) {
+                        $detail = new OrderDetail($detail['id_order_detail']);
+
+                        if(_PS_VERSION_ >= '1.5') {
+                            $detail->unit_price_tax_excl = $detail->unit_price_tax_incl / $rate;
+                            $detail->total_price_tax_excl = $detail->total_price_tax_incl / $rate;
+                            $detail->reduction_amount_tax_excl = $detail->reduction_amount_tax_incl / $rate;
+                            // Update detail tax
+                            $unit_amount = $detail->unit_price_tax_incl - $detail->unit_price_tax_excl;
+                            $total_amount = $detail->total_price_tax_incl -  $detail->total_price_tax_excl;
+
+                            $sql = 'UPDATE `' . _DB_PREFIX_ . 'order_detail_tax`
+                                SET `unit_amount` = ' . $unit_amount . ',
+                                    `total_amount` = ' . $total_amount . '
+                                WHERE `id_order_detail` = ' . $detail->id . '
+                                LIMIT 1';
+
+                            Db::getInstance()->execute($sql);
+                        } else {
+                            $detail->product_price = Tools::ps_round(($detail->product_price * (1 + ($detail->tax_rate / 100))) / $rate, 6);
+                            $detail->tax_rate = Tools::getValue('rate');
+                        }
+
+                        $detail->update();
+                    }
+
+                    // Order Invoice
+                    if(_PS_VERSION_ >= '1.5') {
+                        if($order->hasInvoice()) {
+                            $invoice = new OrderInvoice($order->invoice_number);
+                            $invoice->total_paid_tax_excl = $invoice->total_paid_tax_incl / $rate;
+                            $invoice->total_discount_tax_excl = $invoice->total_discount_tax_incl / $rate;
+                            $invoice->total_products = $invoice->total_products_wt / $rate;
+                            $invoice->update();
+                        }
+                    }
+                }
+                break;
+            default:
+                self::showAvailableAction();
+                break;
+        }
+        exit();
+    }
+
+}
--- webservice/lengow.php
+++ webservice/lengow.php
@@ -21,114 +21,21 @@ require_once '..' . $sep . '..' . $sep . '..' . $sep . 'init.php';
 require_once '..' . $sep . 'lengow.php';
 require_once '..' . $sep . 'models' . $sep . 'lengow.core.class.php';
 require_once '..' . $sep . 'models' . $sep . 'lengow.check.class.php';
+require_once '..' . $sep . 'models' . $sep . 'lengow.webservice.class.php';
 
 $lengow = new Lengow();
 // CheckIP
 if (LengowCore::checkIP()) {
-
-    // Checking configuration
-    if (Tools::getValue('action') == 'check') {
-        if(Tools::getValue('format') == 'json') {
-            header('Content-Type: application/json');
-            echo LengowCheck::getJsonCheckList();
-        } else {
-            echo "<h1>Lengow check configuration<h1>";
-            echo LengowCheck::getHtmlCheckList();
-        }
+    $action = Tools::getValue('action');
+    try {
+        if(LengowWebservice::checkAction($action))
+            LengowWebservice::execute($action);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        echo '<br /><br />';
+        LengowWebservice::showAvailableAction();
     }
-
-    if (Tools::getValue('action') == 'logs') {
-        $days = 10;
-        $show_extra = false;
-
-        if(Tools::getValue('delete') != '') {
-            LengowCore::deleteProcessOrder(Tools::getValue('delete'));
-        }
-
-        if(Tools::getValue('days') != '') {
-            $days = Tools::getValue('days');
-        }
-
-        if(Tools::getValue('show_extra') == 1)
-            $show_extra = true;
-
-        echo LengowCheck::getHtmlLogs($days, $show_extra);
-    }
-
-    if(Tools::getValue('action') == 'tax') {
-        $id_order = Tools::getValue('id_order');
-        $rate = Tools::getValue('rate');
-
-        if($rate == '')
-            die('No rate');
-        if($id_order == '')
-            die('No order in parameters');
-
-        $order = new LengowOrder($id_order);
-        if($order->getTaxesAverageUsed() == 19.6 || $order->getTaxesAverageUsed() == 20) {
-
-            $rate = 1 + ($rate / 100);
-            $order->total_products = Tools::ps_round($order->total_products_wt / $rate, 2);
-            if(_PS_VERSION_ >= '1.5') {
-                $order->total_paid_tax_excl = Tools::ps_round($order->total_paid_tax_incl / $rate, 2);
-                $order->total_shipping_tax_excl = Tools::ps_round($order->total_shipping_tax_incl / $rate, 2);
-                $order->total_wrapping_tax_excl = Tools::ps_round($order->total_wrapping_tax_incl / $rate, 2);
-
-                // Update Order Carrier
-                $sql = 'UPDATE `' . _DB_PREFIX_ . 'order_carrier`
-                        SET `shipping_cost_tax_excl` = `shipping_cost_tax_incl` / ' . $rate . '
-                        WHERE `id_order` = ' . $id_order . '
-                        LIMIT 1';
-                Db::getInstance()->execute($sql);
-            }
-            $order->update();
-
-            // Update Order Detail
-            if(_PS_VERSION_ >= '1.5')
-                $order_detail = $order->getOrderDetailList();
-            else
-                $order_detail = $order->getProductsDetail();
-
-            foreach($order_detail as $detail) {
-                $detail = new OrderDetail($detail['id_order_detail']);
-
-                if(_PS_VERSION_ >= '1.5') {
-                    $detail->unit_price_tax_excl = $detail->unit_price_tax_incl / $rate;
-                    $detail->total_price_tax_excl = $detail->total_price_tax_incl / $rate;
-                    $detail->reduction_amount_tax_excl = $detail->reduction_amount_tax_incl / $rate;
-                    // Update detail tax
-                    $unit_amount = $detail->unit_price_tax_incl - $detail->unit_price_tax_excl;
-                    $total_amount = $detail->total_price_tax_incl -  $detail->total_price_tax_excl;
-
-                    $sql = 'UPDATE `' . _DB_PREFIX_ . 'order_detail_tax`
-                        SET `unit_amount` = ' . $unit_amount . ',
-                            `total_amount` = ' . $total_amount . '
-                        WHERE `id_order_detail` = ' . $detail->id . '
-                        LIMIT 1';
-
-                    Db::getInstance()->execute($sql);
-                } else {
-                    $detail->product_price = Tools::ps_round(($detail->product_price * (1 + ($detail->tax_rate / 100))) / $rate, 6);
-                    $detail->tax_rate = Tools::getValue('rate');
-                }
-
-                $detail->update();
-            }
-
-            // Order Invoice
-            if(_PS_VERSION_ >= '1.5') {
-                if($order->hasInvoice()) {
-                    $invoice = new OrderInvoice($order->invoice_number);
-                    $invoice->total_paid_tax_excl = $invoice->total_paid_tax_incl / $rate;
-                    $invoice->total_discount_tax_excl = $invoice->total_discount_tax_incl / $rate;
-                    $invoice->total_products = $invoice->total_products_wt / $rate;
-                    $invoice->update();
-                }
-            }
-            
-            exit('End Process');
-        }
-    }
+    exit();
 } else {
     die('Unauthorized access for IP : ' . $_SERVER['REMOTE_ADDR']);
 }
